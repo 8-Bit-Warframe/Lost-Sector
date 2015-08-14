b306af93c15cdebebeee157d9652cd81357990c0
Ezard <ezard.ben@gmail.com>
2015-08-14 13:20:19 +0100

    Changed Kubrow to be more self-contained, like other entities

--- com/ezardlabs/lostsector/objects/Kubrow.java
+++ com/ezardlabs/lostsector/objects/Kubrow.java
@@ -1,6 +1,9 @@
 package com.ezardlabs.lostsector.objects;
 
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.Animator;
 import com.ezardlabs.dethsquare.GameObject;
+import com.ezardlabs.dethsquare.TextureAtlas;
 import com.ezardlabs.dethsquare.Vector2;
 import com.ezardlabs.lostsector.Game;
 import com.ezardlabs.lostsector.NavMesh;
@@ -10,6 +13,7 @@ public class Kubrow extends Avatar {
 	private float speed = 10;
 	public boolean attacking = false;
 	TargetType targetType = TargetType.PLAYER;
+	Vector2 target;
 
 	enum TargetType {
 		PLAYER,
@@ -18,6 +22,51 @@ public class Kubrow extends Avatar {
 
 	public Kubrow() {
 		super(3);
+	}
+
+	@Override
+	public void start() {
+		TextureAtlas ta = new TextureAtlas("images/kubrows/white/atlas.png", "images/kubrows/white/atlas.txt");
+		gameObject.renderer.setTextureAtlas(ta, 200, 200);
+		gameObject.animator.setAnimations(new Animation("idle", new TextureAtlas.Sprite[]{ta.getSprite("idle0")}, Animation.AnimationType.ONE_SHOT, Long.MAX_VALUE), new Animation("run",
+				new TextureAtlas.Sprite[]{ta.getSprite("run0"),
+						ta.getSprite("run1"),
+						ta.getSprite("run2"),
+						ta.getSprite("run3"),
+						ta.getSprite("run4"),
+						ta.getSprite("run5"),
+						ta.getSprite("run6")},
+				Animation.AnimationType.LOOP, 100), new Animation("attack",
+				new TextureAtlas.Sprite[]{ta.getSprite("attack0"),
+						ta.getSprite("attack1"),
+						ta.getSprite("attack2"),
+						ta.getSprite("attack3"),
+						ta.getSprite("attack4"),
+						ta.getSprite("attack5"),
+						ta.getSprite("attack6"),
+						ta.getSprite("attack7"),
+						ta.getSprite("attack8"),
+						ta.getSprite("attack9"),
+						ta.getSprite("attack10"),
+						ta.getSprite("attack11")},
+				Animation.AnimationType.ONE_SHOT, 100, new Animation.AnimationListener() {
+			@Override
+			public void onAnimatedStarted(Animator animator) {
+
+			}
+
+			@Override
+			public void onFrame(int frameNum) {
+
+			}
+
+			@Override
+			public void onAnimationFinished(Animator animator) {
+				//noinspection ConstantConditions
+				attacking = false;
+			}
+		}));
+
 	}
 
 	@Override
@@ -40,18 +89,23 @@ public class Kubrow extends Avatar {
 		} else {
 			targetType = TargetType.ENEMY;
 		}
-		switch (targetType) {
-			case PLAYER:
-				break;
-			case ENEMY:
-				break;
+		if (gameObject.rigidbody.gravity == 0) {
+			NavMesh.NavPoint[] path;
+			switch (targetType) {
+				case PLAYER:
+					path = NavMesh.getPath(transform, Game.players[0].transform);
+					break;
+				case ENEMY:
+					break;
+			}
 		}
 		if (best == null) {
 			if (gameObject.rigidbody.gravity == 0) {
 				NavMesh.NavPoint[] path = NavMesh.getPath(transform, Game.players[0].transform);
 				if (path != null && path.length > 1) {
 					int pathIndex = 0;
-					if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x) pathIndex = 1;
+					if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x)
+						pathIndex = 1;
 					if (path[pathIndex].position.x < transform.position.x) {
 						x = -speed;
 						gameObject.renderer.hFlipped = true;
@@ -74,7 +128,8 @@ public class Kubrow extends Avatar {
 					NavMesh.NavPoint[] path = NavMesh.getPath(transform, best.transform);
 					if (path != null && path.length > 0) {
 						int pathIndex = 0;
-						if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x) pathIndex = 1;
+						if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x)
+							pathIndex = 1;
 						if (path[pathIndex].position.x < transform.position.x) {
 							x = -speed;
 							gameObject.renderer.hFlipped = true;
