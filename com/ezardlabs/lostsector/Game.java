36551fb2d05c35e178618de191a974b949251504
Ezard <ezard.ben@gmail.com>
2015-09-08 12:28:00 +0100

    Removed some redundant comments

--- com/ezardlabs/lostsector/Game.java
+++ com/ezardlabs/lostsector/Game.java
@@ -4,15 +4,12 @@ import com.ezardlabs.dethsquare.Animator;
 import com.ezardlabs.dethsquare.Camera;
 import com.ezardlabs.dethsquare.Collider;
 import com.ezardlabs.dethsquare.GameObject;
-import com.ezardlabs.dethsquare.GuiRenderer;
 import com.ezardlabs.dethsquare.Renderer;
 import com.ezardlabs.dethsquare.Rigidbody;
 import com.ezardlabs.dethsquare.Vector2;
 import com.ezardlabs.dethsquare.util.BaseGame;
 import com.ezardlabs.lostsector.objects.CameraMovement;
-import com.ezardlabs.lostsector.objects.Kubrow;
 import com.ezardlabs.lostsector.objects.Player;
-import com.ezardlabs.lostsector.objects.enemies.corpus.crewmen.DeraCrewman;
 import com.ezardlabs.lostsector.objects.warframes.Frost;
 
 public class Game extends BaseGame {
@@ -27,10 +24,10 @@ public class Game extends BaseGame {
 
 	@Override
 	public void create() {
-		MapManager.loadMap("kubrow-map");
+		MapManager.loadMap("map");
 
 		//noinspection RedundantArrayCreation
-		GameObject.instantiate(new GameObject("Dera Crewman", new Renderer(), new Animator(), new Collider(200, 200), new Rigidbody(), new DeraCrewman()), new Vector2(1500, 20));
+//		GameObject.instantiate(new GameObject("Dera Crewman", new Renderer(), new Animator(), new Collider(200, 200), new Rigidbody(), new DeraCrewman()), new Vector2(1500, 20));
 
 //		ta = new TextureAtlas("images/warframes/frost/atlas.png", "images/warframes/frost/atlas.txt");
 		players = new GameObject[]{new GameObject("Player", new Player(), new Renderer(), new Animator(), new Frost(), new Collider(200, 200), new Rigidbody())};
