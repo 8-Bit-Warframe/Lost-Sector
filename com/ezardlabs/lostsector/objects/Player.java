89a39e4e2bc0b562e911f5c5aa443e01bf6ad11e
Ezard <ezard.ben@gmail.com>
2015-09-19 09:50:36 +0100

    Fixed a melee bug

--- com/ezardlabs/lostsector/objects/Player.java
+++ com/ezardlabs/lostsector/objects/Player.java
@@ -1,5 +1,8 @@
 package com.ezardlabs.lostsector.objects;
 
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.Animator;
+import com.ezardlabs.dethsquare.Camera;
 import com.ezardlabs.dethsquare.Collider;
 import com.ezardlabs.dethsquare.Collider.Collision;
 import com.ezardlabs.dethsquare.Collider.CollisionLocation;
@@ -7,26 +10,30 @@ import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.Input;
 import com.ezardlabs.dethsquare.Input.KeyCode;
 import com.ezardlabs.dethsquare.Input.OnTouchListener;
+import com.ezardlabs.dethsquare.Renderer;
 import com.ezardlabs.dethsquare.Screen;
 import com.ezardlabs.dethsquare.Script;
+import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
 import com.ezardlabs.dethsquare.Vector2;
+import com.ezardlabs.dethsquare.animationtypes.OneShotAnimation;
 import com.ezardlabs.dethsquare.util.Utils;
 import com.ezardlabs.lostsector.objects.warframes.Warframe;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Player extends Script {
 	public static GameObject player;
 	public int jumpCount = 0;
 	private int x = 0;
-	public boolean landing = false;
-	public boolean melee = false;
 	private float speed = 12.5f;
 	private Warframe warframe;
 
-	private State state = State.IDLE;
+	public State state = State.IDLE;
 
-	private enum State {
+	public enum State {
 		IDLE,
 		RUNNING,
 		JUMPING,
@@ -34,7 +41,9 @@ public class Player extends Script {
 		FALLING,
 		LANDING,
 		MELEE,
-		SHOOTING
+		MELEE_WAITING,
+		SHOOTING,
+		CASTING
 	}
 
 	@Override
@@ -83,7 +92,8 @@ public class Player extends Script {
 							} else {
 								v.x = x - v.x;
 								v.y = y - v.y;
-								Warframe w = (Warframe) gameObject.getComponentOfType(Warframe.class);
+								Warframe w = (Warframe) gameObject.getComponentOfType(
+										Warframe.class);
 								if (v.x < -150 * Screen.scale) { // left
 									if (w.hasEnergy(25)) {
 										w.removeEnergy(25);
@@ -105,7 +115,7 @@ public class Player extends Script {
 										w.ability2();
 									}
 								} else {
-									jump();
+//									jump();
 								}
 							}
 						}
@@ -121,80 +131,6 @@ public class Player extends Script {
 						map.remove(id);
 					}
 				});
-				break;
-			case DESKTOP:
-//				Input.addOnKeyListener(new OnKeyListener() {
-//					private boolean leftDown = false;
-//					private boolean rightDown = false;
-//					private ArrayList<Character> down = new ArrayList<>();
-//
-//					@Override
-//					public void onKeyTyped(char keyChar) {
-//					}
-//
-//					@SuppressWarnings("ConstantConditions")
-//					@Override
-//					public void onKeyDown(char keyChar) {
-//						if (down.contains(keyChar)) return;
-//						else down.add(keyChar);
-//						Warframe w = (Warframe) gameObject.getComponentOfType(Warframe.class);
-//						switch (keyChar) {
-//							case 'w':
-//							case 'W':
-//							case ' ':
-//							case 'j':
-//							case 'J':
-//								if (!landing && !melee) jump();
-//								break;
-//							case '1':
-//								if (w.hasEnergy(5)) {
-//									w.removeEnergy(5);
-//									w.ability1();
-//								}
-//								break;
-//							case '2':
-//								if (w.hasEnergy(10)) {
-//									w.removeEnergy(10);
-//									w.ability2();
-//								}
-//								break;
-//							case '3':
-//								if (w.hasEnergy(25)) {
-//									w.removeEnergy(25);
-//									w.ability3();
-//								}
-//								break;
-//							case '4':
-//								if (w.hasEnergy(50)) {
-//									w.removeEnergy(50);
-//									w.ability4();
-//								}
-//								break;
-//							case '\n':
-//							case 'k':
-//							case 'K':
-//								if (!landing) melee();
-//								break;
-//						}
-//					}
-//
-//					@Override
-//					public void onKeyUp(char keyChar) {
-//						if (down.contains(keyChar)) down.remove((Character) keyChar);
-//						switch (keyChar) {
-//							case 'a':
-//								leftDown = false;
-//								if (rightDown) x = 1;
-//								else x = 0;
-//								break;
-//							case 'd':
-//								rightDown = false;
-//								if (leftDown) x = -1;
-//								else x = 0;
-//								break;
-//						}
-//					}
-//				});
 				break;
 		}
 		warframe = (Warframe) gameObject.getComponentOfType(Warframe.class);
@@ -212,63 +148,40 @@ public class Player extends Script {
 
 		switch (state) {
 			case IDLE:
-				if (Input.getKeyDown(KeyCode.SPACE)) {
-					state = State.JUMPING;
-					jump();
-					break;
-				}
-				if (gameObject.rigidbody.velocity.y > 0) {
-					state = State.FALLING;
-					break;
-				}
-				if (x != 0) {
-					state = State.RUNNING;
-					break;
-				}
+				if (jumpCheck()) break;
+				if (fallCheck()) break;
+				if (meleeCheck()) break;
+				if (castCheck()) break;
+				if (x != 0) state = State.RUNNING;
 				break;
 			case RUNNING:
-				if (Input.getKeyDown(KeyCode.SPACE)) {
-					state = State.JUMPING;
-					jump();
-					break;
-				}
-				if (gameObject.rigidbody.velocity.y > 0) {
-					state = State.FALLING;
-					break;
-				}
-				if (x == 0) {
-					state = State.IDLE;
-					break;
-				}
+				if (jumpCheck()) break;
+				if (fallCheck()) break;
+				if (meleeCheck()) break;
+				if (castCheck()) break;
+				if (x == 0) state = State.IDLE;
 				break;
 			case JUMPING:
-				if (Input.getKeyDown(KeyCode.SPACE)) {
-					state = State.DOUBLE_JUMPING;
-					jump();
-					break;
-				}
-				if (gameObject.rigidbody.velocity.y > 0) {
-					state = State.FALLING;
-					break;
-				}
+				if (jumpCheck()) break;
+				if (fallCheck()) break;
 				break;
 			case DOUBLE_JUMPING:
-				if (gameObject.rigidbody.velocity.y > 0) {
-					state = State.FALLING;
-					break;
-				}
+				if (fallCheck()) break;
 				break;
 			case FALLING:
+				if (jumpCheck()) break;
 				break;
 			case LANDING:
 				break;
 			case MELEE:
 				break;
+			case MELEE_WAITING:
+				break;
 			case SHOOTING:
 				break;
+			case CASTING:
+				break;
 		}
-
-		System.out.println(state);
 
 		switch (state) {
 			case IDLE:
@@ -277,18 +190,25 @@ public class Player extends Script {
 			case RUNNING:
 				gameObject.animator.play("run");
 				transform.translate(x * speed, 0);
+				if (Input.getKeyDown(KeyCode.ALPHA_1)) warframe.ability1();
+				if (Input.getKeyDown(KeyCode.ALPHA_2)) warframe.ability2();
+				if (Input.getKeyDown(KeyCode.ALPHA_3)) warframe.ability3();
+				if (Input.getKeyDown(KeyCode.ALPHA_4)) warframe.ability4();
 				break;
 			case JUMPING:
 				gameObject.animator.play("jump");
 				transform.translate(x * speed, 0);
+				if (Input.getKeyDown(KeyCode.ALPHA_1)) warframe.ability1();
 				break;
 			case DOUBLE_JUMPING:
 				gameObject.animator.play("doublejump");
 				transform.translate(x * speed, 0);
+				if (Input.getKeyDown(KeyCode.ALPHA_1)) warframe.ability1();
 				break;
 			case FALLING:
 				gameObject.animator.play("fall");
 				transform.translate(x * speed, 0);
+				if (Input.getKeyDown(KeyCode.ALPHA_1)) warframe.ability1();
 				break;
 			case LANDING:
 				gameObject.animator.play("land");
@@ -296,49 +216,62 @@ public class Player extends Script {
 			case MELEE:
 				gameObject.animator.play(warframe.meleeWeapon.getNextAnimation(x));
 				break;
+			case MELEE_WAITING:
+				meleeCheck();
+				break;
 			case SHOOTING:
 				break;
-		}
-
-
-		if (landing) return;
-
-		if (x < 0) {
-			gameObject.renderer.hFlipped = true;
-		} else if (x > 0) {
-			gameObject.renderer.hFlipped = false;
-		}
-
-		if (melee) return;
-
-//		transform.translate(x * speed, 0);
-
-		if (jumpCount > 0 && gameObject.rigidbody.velocity.y > 0) {
-			gameObject.animator.play("fall");
-		}
-
-		if (jumpCount == 0) {
-			if (gameObject.rigidbody.velocity.y > 2) {
-				gameObject.animator.play("fall");
-			} else if (x != 0) {
-				gameObject.animator.play("run");
-			} else {
-				gameObject.animator.play("idle");
-			}
+			case CASTING:
+				gameObject.animator.play("cast");
+				break;
 		}
 	}
 
-	public void jump() {
-		if (!landing && jumpCount++ < 2) {
+	private boolean jumpCheck() {
+		if ((Input.getKeyDown(KeyCode.SPACE) || Input.getKeyDown(KeyCode.J)) && jumpCount++ < 2) {
+			state = jumpCount == 1 ? State.JUMPING : State.DOUBLE_JUMPING;
 			gameObject.rigidbody.velocity.y = -25f;
-			gameObject.animator.play(jumpCount == 1 ? "jump" : "doublejump");
+			return true;
 		}
+		return false;
 	}
 
-	public void melee() {
-		melee = true;
-		//noinspection ConstantConditions
-		gameObject.animator.play(((Warframe) gameObject.getComponentOfType(Warframe.class)).meleeWeapon.getNextAnimation(x));
+	private boolean fallCheck() {
+		if (gameObject.rigidbody.velocity.y > 0) {
+			state = State.FALLING;
+			return true;
+		}
+		return false;
+	}
+
+	private boolean meleeCheck() {
+		if (Input.getKeyDown(KeyCode.RETURN) || Input.getKeyDown(KeyCode.K)) {
+			state = State.MELEE;
+			warframe.meleeWeapon.getNextAnimation(x);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean castCheck() {
+		if (Input.getKeyDown(KeyCode.ALPHA_1)) {
+			warframe.ability1();
+			return false;
+		}
+		if (Input.getKeyDown(KeyCode.ALPHA_2)) {
+			warframe.ability2();
+			return false;
+		}
+		if (Input.getKeyDown(KeyCode.ALPHA_3)) {
+			warframe.ability3();
+			return false;
+		}
+		if ((state == State.IDLE || state == State.RUNNING) && Input.getKeyDown(KeyCode.ALPHA_4)) {
+			state = State.CASTING;
+			warframe.ability4();
+			return true;
+		}
+		return false;
 	}
 
 	@Override
@@ -346,23 +279,22 @@ public class Player extends Script {
 		if (collision.location == CollisionLocation.BOTTOM) {
 			jumpCount = 0;
 			if (collision.speed > 37) {
-//				landing = true;
+				state = State.LANDING;
 //				gameObject.renderer.setSize(200, 200);
 //				gameObject.renderer.setOffsets(0, 0);
 //				gameObject.animator.play("land");
-//				//noinspection ConstantConditions
-//				Camera.main.gameObject.getComponent(CameraMovement.class).startQuake(100, 0.3f);
-//				TextureAtlas ta = new TextureAtlas("images/effects/dust.png", "images/effects/dust.txt");
-//				GameObject.destroy(GameObject.instantiate(new GameObject("Dust", new Renderer(ta, ta.getSprite("dust0"), 700, 50),
-//								new Animator(new Animation("dust", new Sprite[]{ta.getSprite("dust0"), ta.getSprite("dust1"), ta.getSprite("dust2")}, new OneShotAnimation(), 100))),
-//						new Vector2(transform.position.x - 262, transform.position.y + 150)), 300);
-//				new Timer().schedule(new TimerTask() {
-//					@Override
-//					public void run() {
-//						landing = false;
-//					}
-//				}, 300);
-				state = State.LANDING;
+				//noinspection ConstantConditions
+				Camera.main.gameObject.getComponent(CameraMovement.class).startQuake(100, 0.3f);
+				TextureAtlas ta = new TextureAtlas("images/effects/dust.png", "images/effects/dust.txt");
+				GameObject.destroy(GameObject.instantiate(new GameObject("Dust", new Renderer(ta, ta.getSprite("dust0"), 700, 50),
+								new Animator(new Animation("dust", new Sprite[]{ta.getSprite("dust0"), ta.getSprite("dust1"), ta.getSprite("dust2")}, new OneShotAnimation(), 100))),
+						new Vector2(transform.position.x - 262, transform.position.y + 150)), 300);
+				new Timer().schedule(new TimerTask() {
+					@Override
+					public void run() {
+						state = State.IDLE;
+					}
+				}, 300);
 			} else {
 				state = State.IDLE;
 			}
