dd61a6938c1b7315b95af0456b8ca18d9209eac9
Ezard <ezard.ben@gmail.com>
2015-10-14 17:30:20 +0100

    Updated Crewman AI to now shoot at the player if they're in range

--- com/ezardlabs/lostsector/objects/enemies/corpus/Crewman.java
+++ com/ezardlabs/lostsector/objects/enemies/corpus/Crewman.java
@@ -8,6 +8,7 @@ import com.ezardlabs.lostsector.objects.enemies.Enemy;
 
 public class Crewman extends Enemy {
 	Vector2 target;
+	boolean shooting = false;
 
 	public Crewman(String name) {
 		super("corpus/crewmen/" + name, 3);
@@ -17,30 +18,35 @@ public class Crewman extends Enemy {
 	public void update() {
 		int x = 0;
 		if (!landing && gameObject.rigidbody.velocity.y == 0) {
-			if (target == null || transform.position.x == target.x) {
-				NavMesh.NavPoint[] path = NavMesh.getPath(transform, Game.players[0].transform);
-				if (path != null && path.length > 0) {
-					int pathIndex = 0;
-					if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x)
-						pathIndex = 1;
-					target = path[pathIndex].position;
-				}
-			}
-			if (target != null) {
-				if (target.x < transform.position.x) {
-					gameObject.renderer.hFlipped = true;
-				} else if (target.x > transform.position.x) {
-					gameObject.renderer.hFlipped = false;
-				}
-				if (target.y < transform.position.y) {
-					if (gameObject.rigidbody.velocity.y >= 0) {
-						gameObject.rigidbody.velocity.y = -30f;
+			if (transform.position.y == Game.players[0].transform.position.y && Math.abs(transform.position.x - Game.players[0].transform.position.x) < 1500) {
+				shooting = true;
+				gameObject.animator.play("shoot");
+				return;
+			} else {
+				if (target == null || transform.position.x == target.x) {
+					NavMesh.NavPoint[] path = NavMesh.getPath(transform, Game.players[0].transform);
+					if (path != null && path.length > 0) {
+						int pathIndex = 0;
+						if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x) pathIndex = 1;
+						target = path[pathIndex].position;
 					}
-				} else {
+				}
+				if (target != null) {
 					if (target.x < transform.position.x) {
-						x = -10;
+						gameObject.renderer.hFlipped = true;
 					} else if (target.x > transform.position.x) {
-						x = 10;
+						gameObject.renderer.hFlipped = false;
+					}
+					if (target.y < transform.position.y) {
+						if (gameObject.rigidbody.velocity.y >= 0) {
+							gameObject.rigidbody.velocity.y = -30f;
+						}
+					} else {
+						if (target.x < transform.position.x) {
+							x = -10;
+						} else if (target.x > transform.position.x) {
+							x = 10;
+						}
 					}
 				}
 			}
