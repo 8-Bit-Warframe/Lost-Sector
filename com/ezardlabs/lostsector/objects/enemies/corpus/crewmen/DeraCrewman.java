099a4f2637d5dbd373b96c5af60b8818020de572
Ezard <ezard.ben@gmail.com>
2015-10-14 18:41:15 +0100

    Dera Crewman now fires in 4-round bursts, instead of continuously

--- com/ezardlabs/lostsector/objects/enemies/corpus/crewmen/DeraCrewman.java
+++ com/ezardlabs/lostsector/objects/enemies/corpus/crewmen/DeraCrewman.java
@@ -19,7 +19,28 @@ public class DeraCrewman extends Crewman {
 
 	@Override
 	protected Animation getShootAnimation() {
-		return new Animation("shoot", new Sprite[]{ta.getSprite("shoot0"), ta.getSprite("shoot1"), ta.getSprite("shoot2"), ta.getSprite("shoot3")}, AnimationType.LOOP, 100, new AnimationListener() {
+		return new Animation("shoot", new Sprite[]{ta.getSprite("shoot0"), ta.getSprite("shoot1"), ta.getSprite("shoot2"), ta.getSprite("shoot3")}, new AnimationType() {
+			private long lastEnd = 0;
+			private boolean secondSet = false;
+
+			@Override
+			public int update(int currentFrame, int numFrames) {
+				switch (currentFrame) {
+					case 0:
+						if (System.currentTimeMillis() - lastEnd > 2000) return 1;
+						else return 0;
+					case 1:
+					case 2:
+						return currentFrame + 1;
+					case 3:
+						if (secondSet) lastEnd = System.currentTimeMillis();
+						secondSet = !secondSet;
+						return 0;
+					default:
+						return 0;
+				}
+			}
+		}, 100, new AnimationListener() {
 			@Override
 			public void onAnimatedStarted(Animator animator) {
 
@@ -28,9 +49,9 @@ public class DeraCrewman extends Crewman {
 			@Override
 			public void onFrame(Animator animator, int frameNum) {
 				if (frameNum % 2 == 1) {
-					GameObject.instantiate(new GameObject("Laser", new Renderer("images/laser.png", 100, 100).setFlipped(gameObject.renderer.hFlipped, false), new Collider(100, 100, true), new Laser
-									()),
-							transform.position.offset(gameObject.renderer.hFlipped ? -12.5f : 87.5f, frameNum == 1 ? 75 : 50));
+					GameObject
+							.instantiate(new GameObject("Laser", new Renderer("images/laser.png", 100, 100).setFlipped(gameObject.renderer.hFlipped, false), new Collider(100, 100, true), new Laser()),
+									transform.position.offset(gameObject.renderer.hFlipped ? -12.5f : 87.5f, frameNum == 1 ? 75 : 50));
 				}
 			}
 
