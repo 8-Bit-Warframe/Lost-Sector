5858b5af85eb1faf86e648850ca00f58053fdeff
Ezard <ezard.ben@gmail.com>
2015-10-14 18:17:28 +0100

    Added temporary support for easy respawning

--- com/ezardlabs/lostsector/Game.java
+++ com/ezardlabs/lostsector/Game.java
@@ -11,10 +11,13 @@ import com.ezardlabs.dethsquare.util.BaseGame;
 import com.ezardlabs.lostsector.objects.CameraMovement;
 import com.ezardlabs.lostsector.objects.Player;
 import com.ezardlabs.lostsector.objects.enemies.corpus.crewmen.DeraCrewman;
+import com.ezardlabs.lostsector.objects.hud.StatusIndicator;
 import com.ezardlabs.lostsector.objects.warframes.Frost;
 
 public class Game extends BaseGame {
 	public static GameObject[] players;
+	private static CameraMovement cm = new CameraMovement();
+	public static StatusIndicator statusIndicator = new StatusIndicator();
 
 	public enum DamageType {
 		NORMAL,
@@ -27,17 +30,21 @@ public class Game extends BaseGame {
 	public void create() {
 		MapManager.loadMap("map");
 
-		GameObject.instantiate(new GameObject("Dera Crewman", new Renderer(), new Animator(), new Collider(200, 200), new Rigidbody(), new DeraCrewman()), new Vector2(1500, 20));
+		createPlayer();
 
-		players = new GameObject[]{new GameObject("Player", new Player(), new Renderer(), new Animator(), new Frost(), new Collider(200, 200), new Rigidbody())};
-		GameObject.instantiate(players[0], new Vector2(20, 20));
-
-		CameraMovement cm = new CameraMovement();
-		cm.smoothFollow(players[0].transform);
 		GameObject.instantiate(new GameObject("Camera", new Camera(true), cm), new Vector2());
+
+		GameObject.instantiate(new GameObject("Dera Crewman", new Renderer(), new Animator(), new Collider(200, 200), new Rigidbody(), new DeraCrewman()), new Vector2(1500, 20));
 
 //		GameObject.instantiate(new GameObject("Kubrow", new Renderer(), new Animator(), new Collider(200, 200), new Rigidbody(), new Kubrow()), new Vector2(1500, 20));
 
 //		GameObject.instantiate(new GameObject("Buttons"), new Vector2());
+	}
+
+	public static void createPlayer() {
+		players = new GameObject[]{new GameObject("Player", new Player(), new Renderer(), new Animator(), new Frost(), new Collider(200, 200), new Rigidbody())};
+		GameObject.instantiate(players[0], new Vector2(20, 20));
+
+		cm.smoothFollow(players[0].transform);
 	}
 }
