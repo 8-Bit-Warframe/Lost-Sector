90c427f5dee1414cac9d35f5d1184489358c8af0
Benjamin Priyadamkol <benijermin@gmail.com>
2016-04-28 23:57:01 -0400

    Now able to load in lockers, doors and laser doors from TMX files.

--- com/ezardlabs/lostsector/MapManager.java
+++ com/ezardlabs/lostsector/MapManager.java
@@ -1,15 +1,9 @@
 package com.ezardlabs.lostsector;
 
-import com.ezardlabs.dethsquare.Animation;
-import com.ezardlabs.dethsquare.AnimationType;
-import com.ezardlabs.dethsquare.Animator;
-import com.ezardlabs.dethsquare.Collider;
-import com.ezardlabs.dethsquare.GameObject;
-import com.ezardlabs.dethsquare.Renderer;
-import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.*;
 import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
-import com.ezardlabs.dethsquare.Vector2;
 import com.ezardlabs.dethsquare.tmx.*;
+import com.ezardlabs.lostsector.objects.enemies.corpus.Crewman;
 import com.ezardlabs.lostsector.objects.environment.Camera;
 import com.ezardlabs.lostsector.objects.environment.Door;
 import com.ezardlabs.lostsector.objects.environment.Locker;
@@ -17,9 +11,6 @@ import com.ezardlabs.lostsector.objects.environment.Locker;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import java.io.*;
 import java.util.ArrayList;
 
 public class MapManager {
@@ -171,26 +162,34 @@ public class MapManager {
 		for(TileSet tileSet : tileSets) {
 			textureAtlases.add(new TextureAtlas("maps/" + tileSet.getImageSource(), tileSet.getTileWidth(), tileSet.getTileHeight()));
 		}
-		Tile[] tiles = null;
 		float w = map.getTileWidth() * 6.25f;
 		float h = map.getTileHeight() * 6.25f;
-		// Background layer with no collision
-		for(Layer layer : map.getBackgroundLayers()) {
-			tiles = layer.getTiles();
-			instantiateTiles(tiles, false, w, h, -10, map, tileSets, textureAtlases);
+		boolean isBackgroundLayer = true;
+		for(Layer layer : map.getTileLayers()) {
+			Tile[] tiles = layer.getTiles();
+			if(layer.getName().equals("main")) {
+				// Main layer with collision
+				instantiateTiles(tiles, true, w, h, 5, map, tileSets, textureAtlases);
+				isBackgroundLayer = false;
+			} else if(isBackgroundLayer) {
+				// Background layer with no collision
+				instantiateTiles(tiles, false, w, h, -10, map, tileSets, textureAtlases);
+			} else if(!isBackgroundLayer) {
+				// Foreground layer with no collision
+				instantiateTiles(tiles, false, w, h, 10, map, tileSets, textureAtlases);
+			}
 		}
-		// Main Layer with collision
-		instantiateTiles(map.getMainLayer().getTiles(), true, w, h, 5, map, tileSets, textureAtlases);
-		// TODO: Foreground layers not working as intended. (Still drawing behind player)
-		for(Layer layer : map.getForegroundLayers()) {
-			tiles = layer.getTiles();
-			instantiateTiles(tiles, false, w, h, 10, map, tileSets, textureAtlases);
+
+		TextureAtlas ta = new TextureAtlas("images/environment/atlas.png", "images/environment/atlas.txt");
+		for(ObjectGroup objectGroup : map.getObjectGroups()) {
+			instantiateObjects(objectGroup.getObjects(), ta);
 		}
 	}
 
 	public static void instantiateTiles(Tile[] tiles, boolean collision, float tileWidth, float tileHeight, int zindex, Map map, ArrayList<TileSet> tileSets, ArrayList<TextureAtlas> textureAtlases) {
 		for (int i = 0; i < tiles.length; i++) {
-			int gid = tiles[i].getGid();
+			Tile t = tiles[i];
+			long gid = t.getGid();
 			if(gid <= 0) {
 				continue;
 			}
@@ -213,7 +212,7 @@ public class MapManager {
 			}
 			TextureAtlas ta = textureAtlases.get(tileSetIdx);
 			Sprite sprite = ta.getSprite(String.valueOf(gid - ts.getFirstGid()));
-			Renderer renderer = new Renderer(ta, sprite, tileWidth, tileHeight).setzIndex(zindex);
+			Renderer renderer = new Renderer(ta, sprite, tileWidth, tileHeight).setzIndex(zindex).setFlipped(t.isFlippedHorizontal(), t.isFlippedVertical());
 			if(collision) {
 				GameObject.instantiate(
 						new GameObject("Tile", true, renderer, new Collider(tileWidth, tileHeight)),
@@ -224,6 +223,76 @@ public class MapManager {
 						new GameObject("Tile", true, renderer),
 						new Vector2(x, y)
 				);
+			}
+		}
+	}
+
+	public static void instantiateObjects(TMXObject[] objects, TextureAtlas ta) {
+		for(TMXObject object : objects) {
+			Vector2 pos = new Vector2(object.getX() * 6.25f, object.getY() * 6.25f);
+			float w = object.getWidth() * 6.25f;
+			float h = object.getHeight() * 6.25f;
+			Collider collider = new Collider(object.getWidth() * 6.25f, object.getHeight() * 6.25f);
+			switch(object.getType()) {
+				case "crewman":
+//					GameObject.instantiate(
+//							new GameObject("Crewman", new Renderer(), new Animator(), new Collider(object.getWidth() * 6.25f, object.getHeight() * 6.25f), new Rigidbody(), new Crewman("dera")),
+//							pos);
+					break;
+				case "locker":
+					if(0 + (int)(Math.random() * ((1 - 0) + 1)) == 0)
+						GameObject.instantiate(new GameObject("Locker", true, new Locker(true), new Renderer(ta, ta.getSprite("lockred"), 100, 200)), pos);
+					else
+						GameObject.instantiate(new GameObject("Locker", true, new Locker(false), new Renderer(ta, ta.getSprite("lock0"), 100, 200), new Animator(
+							new Animation("unlock", new Sprite[]{ta.getSprite("lock1"), ta.getSprite("lock2"), ta.getSprite("lock3"), ta.getSprite("lock4"), ta.getSprite("lock5")}, AnimationType.ONE_SHOT,
+									125)), new Collider(100, 200, true)), pos);
+					break;
+				case "locker_locked":
+					GameObject.instantiate(new GameObject("Locker", true, new Locker(true), new Renderer(ta, ta.getSprite("lockred"), 100, 200)), pos);
+					break;
+				case "locker_unlocked":
+					GameObject.instantiate(new GameObject("Locker", true, new Locker(false), new Renderer(ta, ta.getSprite("lock0"), 100, 200), new Animator(
+							new Animation("unlock", new Sprite[]{ta.getSprite("lock1"), ta.getSprite("lock2"), ta.getSprite("lock3"), ta.getSprite("lock4"), ta.getSprite("lock5")}, AnimationType.ONE_SHOT,
+									125)), new Collider(100, 200, true)), pos);
+					break;
+				case "door":
+					GameObject.instantiate(new GameObject("Door", new Door(), new Collider(w * 0.5f, h, true), new Renderer(ta, ta.getSprite("door0"), w, h),
+							new Animator(new Animation("open", new Sprite[]{ta.getSprite("door0"),
+									ta.getSprite("door1"),
+									ta.getSprite("door2"),
+									ta.getSprite("door3"),
+									ta.getSprite("door4"),
+									ta.getSprite("door5"),
+									ta.getSprite("door6")}, AnimationType.ONE_SHOT, 80), new Animation("close", new Sprite[]{ta.getSprite("door6"),
+									ta.getSprite("door5"),
+									ta.getSprite("door4"),
+									ta.getSprite("door3"),
+									ta.getSprite("door2"),
+									ta.getSprite("door1"),
+									ta.getSprite("door0")}, AnimationType.ONE_SHOT, 80))), pos);
+//					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(64 * 3.125f, 32 * 3.125f)), new Vector2(x, y));
+//					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(64 * 3.125f, 32 * 3.125f)), new Vector2(x, y + 128 * 3.125f));
+					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(w, h * 0.2f)), pos);
+					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(w, h * 0.2f)), new Vector2(pos.x, pos.y + h * 0.8f));
+					break;
+				case "ldoor":
+					GameObject.instantiate(new GameObject("Door", new Door(), new Collider(w * 0.5f, h, true), new Renderer(ta, ta.getSprite("ldoor0"), w, h),
+							new Animator(new Animation("open", new Sprite[]{ta.getSprite("ldoor")}, AnimationType.ONE_SHOT, 80), new Animation("close", new Sprite[]{ta.getSprite("ldoor0"),
+									ta.getSprite("ldoor1"),
+									ta.getSprite("ldoor2"),
+									ta.getSprite("ldoor3"),
+									ta.getSprite("ldoor4"),
+									ta.getSprite("ldoor5"),
+									ta.getSprite("ldoor6"),
+									ta.getSprite("ldoor7"),
+									ta.getSprite("ldoor8"),
+									ta.getSprite("ldoor9"),
+									ta.getSprite("ldoor10")}, AnimationType.OSCILLATE, 80))), pos);
+//					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(64 * 3.125f, 32 * 3.125f)), new Vector2(x, y));
+//					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(64 * 3.125f, 32 * 3.125f)), new Vector2(x, y + 128 * 3.125f));
+					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(w, h * 0.2f)), pos);
+					GameObject.instantiate(new GameObject("DoorCollider", true, new Collider(w, h * 0.2f)), new Vector2(pos.x, pos.y + h * 0.8f));
+					break;
 			}
 		}
 	}
