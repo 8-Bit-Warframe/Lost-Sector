fbcb91e3c3fc7750faa864f3a2314ffcc4e6f735
Ezard <ezard.ben@gmail.com>
2016-08-17 11:14:59 +0100

    Add some work to the Shockwave Moa

--- src/main/java/com/ezardlabs/lostsector/objects/enemies/corpus/Moa.java
+++ src/main/java/com/ezardlabs/lostsector/objects/enemies/corpus/Moa.java
@@ -1,10 +1,113 @@
 package com.ezardlabs.lostsector.objects.enemies.corpus;
 
-import com.ezardlabs.lostsector.objects.Avatar;
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.Animation.AnimationListener;
+import com.ezardlabs.dethsquare.AnimationType;
+import com.ezardlabs.dethsquare.Animator;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
+import com.ezardlabs.lostsector.objects.enemies.Enemy;
 
-public class Moa extends Avatar {
+public class Moa extends Enemy {
 
-	public Moa() {
-		super(3);
+	public Moa(String name) {
+		super("corpus/moa/" + name, 2);
+	}
+
+	protected Animation getRunAnimation() {
+		return new Animation("run", new Sprite[]{ta.getSprite("run0"),
+				ta.getSprite("run1"),
+				ta.getSprite("run2"),
+				ta.getSprite("run3"),
+				ta.getSprite("run4"),
+				ta.getSprite("run5")}, AnimationType.LOOP, 100);
+	}
+
+	protected Animation getLandAnimation() {
+		return new Animation("land", new Sprite[]{ta.getSprite("land0")}, AnimationType.ONE_SHOT,
+				100, new AnimationListener() {
+			@Override
+			public void onAnimatedStarted(Animator animator) {
+			}
+
+			@Override
+			public void onFrame(Animator animator, int frameNum) {
+			}
+
+			@Override
+			public void onAnimationFinished(Animator animator) {
+				landing = false;
+			}
+		});
+	}
+
+	protected Animation getShootAnimation() {
+		return new Animation("shoot", new Sprite[]{ta.getSprite("shoot0"),
+				ta.getSprite("shoot1")}, AnimationType.LOOP, 100);
+	}
+
+	private Animation getDieAnimation(String name) {
+		return new Animation(name, new Sprite[]{ta.getSprite("die0"),
+				ta.getSprite("die1"),
+				ta.getSprite("die2"),
+				ta.getSprite("die3"),
+				ta.getSprite("die4"),
+				ta.getSprite("die5"),
+				ta.getSprite("die6")}, AnimationType.ONE_SHOT, 100);
+	}
+
+	protected Animation getDieShootFrontAnimation() {
+		return getDieAnimation("die_shoot_front");
+	}
+
+	protected Animation getDieShootBackAnimation() {
+		return getDieAnimation("die_shoot_back");
+	}
+
+	protected Animation getDieSlashFrontAnimation() {
+		return getDieAnimation("die_slash_front");
+	}
+
+	protected Animation getDieSlashBackAnimation() {
+		return getDieAnimation("die_slash_back");
+	}
+
+	protected Animation getDieKubrowFrontAnimation() {
+		return getDieAnimation("die_kubrow_front");
+	}
+
+	protected Animation getDieKubrowBackAnimation() {
+		return getDieAnimation("die_kubrow_back");
+	}
+
+	protected Animation getFrozenShatterAnimation() {
+		return new Animation("frozen_shatter", new Sprite[]{ta.getSprite("frozen_shatter0"),
+				ta.getSprite("frozen_shatter1"),
+				ta.getSprite("frozen_shatter2"),
+				ta.getSprite("frozen_shatter3"),
+				ta.getSprite("frozen_shatter4"),
+				ta.getSprite("frozen_shatter5"),
+				ta.getSprite("frozen_shatter6"),
+				ta.getSprite("frozen_shatter7"),
+				ta.getSprite("frozen_shatter8"),
+				ta.getSprite("frozen_shatter9")}, AnimationType.ONE_SHOT, 100,
+				new AnimationListener() {
+					@Override
+					public void onAnimatedStarted(Animator animator) {
+						if (!gameObject.renderer.hFlipped) {
+							transform.translate(-100, 0);
+						}
+						gameObject.renderer.setSize(300, 200);
+					}
+
+					@Override
+					public void onFrame(Animator animator, int frameNum) {
+
+					}
+
+					@Override
+					public void onAnimationFinished(Animator animator) {
+
+					}
+				});
 	}
 }
---
+++ src/main/java/com/ezardlabs/lostsector/objects/enemies/corpus/moas/ShockwaveMoa.java
@@ -0,0 +1,155 @@
+package com.ezardlabs.lostsector.objects.enemies.corpus.moas;
+
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.Animation.AnimationListener;
+import com.ezardlabs.dethsquare.AnimationType;
+import com.ezardlabs.dethsquare.Animator;
+import com.ezardlabs.dethsquare.Collider;
+import com.ezardlabs.dethsquare.Component;
+import com.ezardlabs.dethsquare.GameObject;
+import com.ezardlabs.dethsquare.Renderer;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
+import com.ezardlabs.dethsquare.Vector2;
+import com.ezardlabs.lostsector.Game;
+import com.ezardlabs.lostsector.NavMesh;
+import com.ezardlabs.lostsector.objects.enemies.corpus.Moa;
+
+public class ShockwaveMoa extends Moa {
+	private Animator animator;
+	private boolean stomping = false;
+	private boolean shooting = false;
+	private Vector2 target;
+
+	public ShockwaveMoa() {
+		super("shockwave");
+	}
+
+	@Override
+	public void start() {
+		super.start();
+		animator = gameObject.getComponent(Animator.class);
+		animator.addAnimations(new Animation("stomp", new Sprite[]{ta.getSprite("stomp0"),
+				ta.getSprite("stomp1"),
+				ta.getSprite("stomp2"),
+				ta.getSprite("stomp3"),
+				ta.getSprite("stomp4"),
+				ta.getSprite("stomp5"),
+				ta.getSprite("stomp6"),
+				ta.getSprite("stomp7"),
+				ta.getSprite("stomp8"),
+				ta.getSprite("stomp9")}, AnimationType.ONE_SHOT, 100, new AnimationListener() {
+			@Override
+			public void onAnimatedStarted(Animator animator) {
+
+			}
+
+			@Override
+			public void onFrame(Animator animator, int frameNum) {
+				if (frameNum == 7) {
+					GameObject.instantiate(new GameObject("Shockwave", new Renderer(), new Animator(
+							new Animation("shockwave", new Sprite[]{ta.getSprite("shockwave0"),
+									ta.getSprite("shockwave1"),
+									ta.getSprite("shockwave2"),
+									ta.getSprite("shockwave3"),
+									ta.getSprite("shockwave4")}, AnimationType.ONE_SHOT, 100,
+									new AnimationListener() {
+										@Override
+										public void onAnimatedStarted(Animator animator) {
+
+										}
+
+										@Override
+										public void onFrame(Animator animator, int frameNum) {
+
+										}
+
+										@Override
+										public void onAnimationFinished(Animator animator) {
+											GameObject.destroy(animator.gameObject);
+										}
+									})), new Component() {
+						@Override
+						public void start() {
+							this.gameObject.getComponent(Animator.class).play("shockwave");
+						}
+					}), new Vector2(transform.position.x - 300, transform.position.y + 200));
+				}
+			}
+
+			@Override
+			public void onAnimationFinished(Animator animator) {
+				stomping = false;
+			}
+		}));
+		System.out.println(gameObject.renderer.textureName);
+	}
+
+	@Override
+	public void update() {
+		if (dead || frozen || stomping) return;
+		for (GameObject player : Game.players) {
+			if (Vector2.distance(transform.position, player.transform.position) < 300) {
+				animator.play("stomp");
+				stomping = true;
+			}
+		}
+		int x = 0;
+		if (!landing && gameObject.rigidbody.velocity.y == 0) {
+			if (Game.players.length > 0 && transform.position.y == Game.players[0].transform.position.y && Math.abs(transform.position.x - Game.players[0].transform.position.x) < 1500) {
+				shooting = true;
+				gameObject.animator.play("shoot");
+				if (Game.players[0].transform.position.x < transform.position.x) {
+					gameObject.renderer.hFlipped = true;
+				} else if (Game.players[0].transform.position.x > transform.position.x) {
+					gameObject.renderer.hFlipped = false;
+				}
+				return;
+			} else {
+				if ((target == null || transform.position.x == target.x) && Game.players.length > 0) {
+					NavMesh.NavPoint[] path = NavMesh.getPath(transform, Game.players[0].transform);
+					if (path != null && path.length > 0) {
+						int pathIndex = 0;
+						if (path.length > 1 && path[0] != null && transform.position.x == path[0].position.x) pathIndex = 1;
+						target = path[pathIndex].position;
+					}
+				}
+				if (target != null) {
+					if (target.x < transform.position.x) {
+						gameObject.renderer.hFlipped = true;
+					} else if (target.x > transform.position.x) {
+						gameObject.renderer.hFlipped = false;
+					}
+					if (target.y - 100 < transform.position.y) {
+						if (gameObject.rigidbody.velocity.y >= 0) {
+							gameObject.rigidbody.velocity.y = -30f;
+						}
+					} else {
+						if (target.x < transform.position.x) {
+							x = -10;
+						} else if (target.x > transform.position.x) {
+							x = 10;
+						}
+					}
+				}
+			}
+		}
+		transform.translate(x, 0);
+		if (landing) {
+			gameObject.animator.play("land");
+		} else if (gameObject.rigidbody.velocity.y > 0) {
+			gameObject.animator.play("fall");
+		} else if (x != 0) {
+			gameObject.animator.play("run");
+		} else {
+			gameObject.animator.play("idle");
+		}
+	}
+
+	@Override
+	public void onCollision(Collider other, Collider.Collision collision) {
+		if (collision.speed > 37 && collision.location == Collider.CollisionLocation.BOTTOM) {
+			gameObject.animator.play("land");
+			landing = true;
+		}
+	}
+}
