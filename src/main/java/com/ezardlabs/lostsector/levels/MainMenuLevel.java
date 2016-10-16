24b8f2d1f3e40363db1015e81eeeaa0199b17aab
Ezard <ezard.ben@gmail.com>
2016-10-16 21:44:53 +0100

    Add music to title screen

--- src/main/java/com/ezardlabs/lostsector/levels/MainMenuLevel.java
+++ src/main/java/com/ezardlabs/lostsector/levels/MainMenuLevel.java
@@ -1,17 +1,18 @@
 package com.ezardlabs.lostsector.levels;
 
+import com.ezardlabs.dethsquare.AudioSource;
+import com.ezardlabs.dethsquare.AudioSource.AudioClip;
 import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.Level;
 import com.ezardlabs.dethsquare.Vector2;
 import com.ezardlabs.lostsector.objects.menus.MainMenu;
 
-/**
- * Created by Benjamin on 2016-07-04.
- */
 public class MainMenuLevel extends Level {
 
-    @Override
-    public void onLoad() {
-        GameObject.instantiate(new GameObject("MainMenu", new MainMenu()), new Vector2());
-    }
+	@Override
+	public void onLoad() {
+		GameObject.instantiate(new GameObject("MainMenu", new MainMenu(),
+						new AudioSource(new AudioClip("audio/this_is_what_you_are.ogg"), true, 50)),
+				new Vector2());
+	}
 }
