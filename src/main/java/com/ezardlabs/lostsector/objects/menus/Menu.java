77297d1916dc3f51493e7030ecfcb865931cb444
Ezard <ezard.ben@gmail.com>
2016-10-17 05:33:45 +0100

    Add option to offset menu from the centre of the screen

--- src/main/java/com/ezardlabs/lostsector/objects/menus/Menu.java
+++ src/main/java/com/ezardlabs/lostsector/objects/menus/Menu.java
@@ -12,6 +12,7 @@ import com.ezardlabs.dethsquare.Vector2;
 public class Menu extends Script {
 	private final String[] options;
 	private final MenuAction[] actions;
+	private final Vector2 offset;
 
 	private TextureAtlas font;
 
@@ -20,8 +21,13 @@ public class Menu extends Script {
 	private GuiText[] guiTexts;
 
 	public Menu(String[] options, MenuAction[] actions) {
+		this(options, actions, new Vector2());
+	}
+
+	public Menu(String[] options, MenuAction[] actions, Vector2 offset) {
 		this.options = options;
 		this.actions = actions;
+		this.offset = offset;
 
 		if (options.length < 1 || options.length > 4) {
 			throw new Error("You must supply between 1 and 4 options to the menu");
@@ -46,15 +52,15 @@ public class Menu extends Script {
 		float height = (2 * options.length + 1) * 50;
 		gameObject = new GameObject("Menu",
 				new Renderer("images/menus/main/menu" + options.length + ".png", 400, height));
-		GameObject.instantiate(gameObject,
-				new Vector2(1920 / 2 - 200, 1080 / 2 - ((2 * options.length + 1) * 50) / 2));
+		GameObject.instantiate(gameObject, new Vector2(1920 / 2 - 200 + offset.x,
+				1080 / 2 - ((2 * options.length + 1) * 50) / 2 + offset.y));
 		texts = new GameObject[options.length];
 		guiTexts = new GuiText[options.length];
 		for (int i = 0; i < texts.length; i++) {
 			guiTexts[i] = new GuiText(options[i], font, 50);
 			texts[i] = GameObject.instantiate(new GameObject("Menu Option", guiTexts[i]),
-					new Vector2(1920 / 2f - guiTexts[i].getWidth() / 2f,
-							1080 / 2 - height / 2 + (1 + i * 2) * 50));
+					new Vector2(1920 / 2f - guiTexts[i].getWidth() / 2f + offset.x,
+							1080 / 2 - height / 2 + (1 + i * 2) * 50 + offset.y));
 		}
 	}
 
