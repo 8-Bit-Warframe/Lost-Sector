1365eed919012354bb82f92616ed414367629ca7
Ezard <ezard.ben@gmail.com>
2016-11-07 02:08:27 +0000

    Move prefab registration into game setup

--- src/main/java/com/ezardlabs/lostsector/Game.java
+++ src/main/java/com/ezardlabs/lostsector/Game.java
@@ -1,7 +1,15 @@
 package com.ezardlabs.lostsector;
 
+import com.ezardlabs.dethsquare.Animator;
+import com.ezardlabs.dethsquare.Collider;
 import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.LevelManager;
+import com.ezardlabs.dethsquare.Renderer;
+import com.ezardlabs.dethsquare.Rigidbody;
+import com.ezardlabs.dethsquare.multiplayer.Network;
+import com.ezardlabs.dethsquare.multiplayer.NetworkAnimator;
+import com.ezardlabs.dethsquare.multiplayer.NetworkTransform;
+import com.ezardlabs.dethsquare.prefabs.PrefabManager;
 import com.ezardlabs.dethsquare.util.BaseGame;
 import com.ezardlabs.lostsector.levels.ExploreLevel;
 import com.ezardlabs.lostsector.levels.MainMenuLevel;
@@ -9,6 +17,8 @@ import com.ezardlabs.lostsector.levels.MultiplayerLevel;
 import com.ezardlabs.lostsector.levels.MultiplayerLobbyLevel;
 import com.ezardlabs.lostsector.levels.ProceduralLevel;
 import com.ezardlabs.lostsector.levels.SurvivalLevel;
+import com.ezardlabs.lostsector.objects.Player;
+import com.ezardlabs.lostsector.objects.warframes.Frost;
 
 public class Game extends BaseGame {
 	public static GameObject[] players;
@@ -29,6 +39,17 @@ public class Game extends BaseGame {
 		LevelManager.registerLevel("multiplayer", new MultiplayerLevel());
 		LevelManager.registerLevel("main_menu", new MainMenuLevel());
 
+		registerPlayerPrefabs();
+
 		LevelManager.loadLevel("main_menu");
 	}
+
+	private void registerPlayerPrefabs() {
+		PrefabManager.addPrefab("player",
+				() -> new GameObject("Player " + Network.getPlayerId(), new Player(),
+						new Renderer(), new Animator(), new Frost(), new Collider(200, 200),
+						new Rigidbody(), new NetworkTransform(), new NetworkAnimator()),
+				() -> new GameObject("Player " + Network.getPlayerId(), new Renderer(),
+						new Animator(), new Frost(), new NetworkTransform(),
+						new NetworkAnimator()));}
 }
--- src/main/java/com/ezardlabs/lostsector/levels/MultiplayerLevel.java
+++ src/main/java/com/ezardlabs/lostsector/levels/MultiplayerLevel.java
@@ -1,35 +1,18 @@
 package com.ezardlabs.lostsector.levels;
 
-import com.ezardlabs.dethsquare.Animator;
 import com.ezardlabs.dethsquare.Camera;
-import com.ezardlabs.dethsquare.Collider;
 import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.Level;
-import com.ezardlabs.dethsquare.Renderer;
-import com.ezardlabs.dethsquare.Rigidbody;
 import com.ezardlabs.dethsquare.Vector2;
 import com.ezardlabs.dethsquare.multiplayer.Network;
-import com.ezardlabs.dethsquare.multiplayer.NetworkAnimator;
-import com.ezardlabs.dethsquare.multiplayer.NetworkTransform;
-import com.ezardlabs.dethsquare.prefabs.PrefabManager;
 import com.ezardlabs.lostsector.map.MapManager;
 import com.ezardlabs.lostsector.objects.CameraMovement;
-import com.ezardlabs.lostsector.objects.Player;
 import com.ezardlabs.lostsector.objects.hud.HUD;
-import com.ezardlabs.lostsector.objects.warframes.Frost;
 
 public class MultiplayerLevel extends Level {
 
 	@Override
 	public void onLoad() {
-		PrefabManager.addPrefab("player",
-				() -> new GameObject("Player " + Network.getPlayerId(), new Player(),
-						new Renderer(), new Animator(), new Frost(), new Collider(200, 200),
-						new Rigidbody(), new NetworkTransform(), new NetworkAnimator()),
-				() -> new GameObject("Player " + Network.getPlayerId(), new Renderer(),
-						new Animator(), new Frost(), new NetworkTransform(),
-						new NetworkAnimator()));
-
 		MapManager.playerSpawn = new Vector2(20, 20);
 		MapManager.loadMap("test");
 
