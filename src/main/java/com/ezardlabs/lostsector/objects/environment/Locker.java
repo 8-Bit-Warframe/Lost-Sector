11fba1be583963208f193c21696f74fb38e10c76
Ezard <ezard.ben@gmail.com>
2016-11-15 07:17:42 +0000

    Make locker objects self-contained

--- src/main/java/com/ezardlabs/lostsector/objects/environment/Locker.java
+++ src/main/java/com/ezardlabs/lostsector/objects/environment/Locker.java
@@ -1,16 +1,39 @@
 package com.ezardlabs.lostsector.objects.environment;
 
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.AnimationType;
 import com.ezardlabs.dethsquare.Animator;
 import com.ezardlabs.dethsquare.Collider;
 import com.ezardlabs.dethsquare.Component;
+import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
 import com.ezardlabs.lostsector.objects.warframes.Warframe;
 
 public class Locker extends Component {
 	private final boolean locked;
+	private final TextureAtlas ta;
 	private boolean unlocking = false;
 
-	public Locker(boolean locked) {
+	public Locker(boolean locked, TextureAtlas ta) {
 		this.locked = locked;
+		this.ta = ta;
+	}
+
+	@Override
+	public void start() {
+		super.start();
+		gameObject.renderer.setTextureAtlas(ta, 100, 200);
+		if (locked) {
+			gameObject.renderer.setSprite(ta.getSprite("lockred"));
+		} else {
+			gameObject.renderer.setSprite(ta.getSprite("lock0"));
+			gameObject.animator.addAnimations(new Animation("unlock",
+					new Sprite[]{ta.getSprite("lock1"),
+							ta.getSprite("lock2"),
+							ta.getSprite("lock3"),
+							ta.getSprite("lock4"),
+							ta.getSprite("lock5")}, AnimationType.ONE_SHOT, 125));
+		}
 	}
 
 	@Override
