2f5722d152d261293f96b83b06cfeb37d22e65ed
Ezard <ezard.ben@gmail.com>
2016-11-16 06:25:41 +0000

    Add default to switch statement

--- src/main/java/com/ezardlabs/lostsector/objects/CameraMovement.java
+++ src/main/java/com/ezardlabs/lostsector/objects/CameraMovement.java
@@ -56,6 +56,8 @@ public class CameraMovement extends Script {
 						transform.position.y += dy / dn * (dn * 0.2f);
 					}
 					break;
+				default:
+					break;
 			}
 		}
 		if (isQuaking) updateQuake();
