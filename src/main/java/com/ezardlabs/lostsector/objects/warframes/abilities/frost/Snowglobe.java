da16b78fbd36014375bc2232f033b34dd1e54d20
Ezard <ezard.ben@gmail.com>
2016-11-24 20:34:33 +0000

    Formatting

--- src/main/java/com/ezardlabs/lostsector/objects/warframes/abilities/frost/Snowglobe.java
+++ src/main/java/com/ezardlabs/lostsector/objects/warframes/abilities/frost/Snowglobe.java
@@ -10,26 +10,28 @@ public class Snowglobe extends Component {
 
 	@Override
 	public void start() {
-		TextureAtlas ta = new TextureAtlas("images/warframes/abilities/frost/snowglobe/atlas.png", "images/warframes/abilities/frost/snowglobe/atlas.txt");
+		TextureAtlas ta = new TextureAtlas("images/warframes/abilities/frost/snowglobe/atlas.png",
+				"images/warframes/abilities/frost/snowglobe/atlas.txt");
 		gameObject.renderer.setTextureAtlas(ta, 800, 600);
-		gameObject.animator.setAnimations(new Animation("snowglobe", new TextureAtlas.Sprite[]{ta.getSprite("sg0"),
-				ta.getSprite("sg1"),
-				ta.getSprite("sg2"),
-				ta.getSprite("sg3"),
-				ta.getSprite("sg4"),
-				ta.getSprite("sg5"),
-				ta.getSprite("sg6"),
-				ta.getSprite("sg7"),
-				ta.getSprite("sg8"),
-				ta.getSprite("sg9"),
-				ta.getSprite("sg10"),
-				ta.getSprite("sg11"),
-				ta.getSprite("sg12"),
-				ta.getSprite("sg13"),
-				ta.getSprite("sg14"),
-				ta.getSprite("sg15"),
-				ta.getSprite("sg16"),
-				ta.getSprite("sg17")}, AnimationType.LOOP, 100));
+		gameObject.animator.setAnimations(new Animation("snowglobe",
+				new TextureAtlas.Sprite[]{ta.getSprite("sg0"),
+						ta.getSprite("sg1"),
+						ta.getSprite("sg2"),
+						ta.getSprite("sg3"),
+						ta.getSprite("sg4"),
+						ta.getSprite("sg5"),
+						ta.getSprite("sg6"),
+						ta.getSprite("sg7"),
+						ta.getSprite("sg8"),
+						ta.getSprite("sg9"),
+						ta.getSprite("sg10"),
+						ta.getSprite("sg11"),
+						ta.getSprite("sg12"),
+						ta.getSprite("sg13"),
+						ta.getSprite("sg14"),
+						ta.getSprite("sg15"),
+						ta.getSprite("sg16"),
+						ta.getSprite("sg17")}, AnimationType.LOOP, 100));
 		gameObject.animator.play("snowglobe");
 		gameObject.renderer.setzIndex(4);
 		gameObject.setTag("solid");
