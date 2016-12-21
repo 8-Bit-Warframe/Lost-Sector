2feff1e9d3c24ccd8811088b94187c6be1e7f282
Ezard <ezard.ben@gmail.com>
2016-12-21 22:16:28 +0000

    Add Santa prova crewman

---
+++ src/main/java/com/ezardlabs/lostsector/objects/enemies/christmas/corpus/crewmen/SantaProvaCrewman.java
@@ -0,0 +1,24 @@
+package com.ezardlabs.lostsector.objects.enemies.christmas.corpus.crewmen;
+
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.AnimationType;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
+import com.ezardlabs.lostsector.objects.enemies.corpus.crewmen.ProvaCrewman;
+
+public class SantaProvaCrewman extends ProvaCrewman {
+
+	@Override
+	protected String getAtlasPath() {
+		return "christmas/corpus/crewmen/santa_prova";
+	}
+
+	@Override
+	protected Animation getDieKubrowFrontAnimation() {
+		return new Animation("die_kubrow_front", new Sprite[]{}, AnimationType.ONE_SHOT, 0);
+	}
+
+	@Override
+	protected Animation getDieKubrowBackAnimation() {
+		return new Animation("die_kubrow_back", new Sprite[]{}, AnimationType.ONE_SHOT, 0);
+	}
+}
