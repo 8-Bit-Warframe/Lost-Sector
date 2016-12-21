7259fc561e16bc549346dbda09d8cac6d40a0f74
Ezard <ezard.ben@gmail.com>
2016-12-21 22:41:38 +0000

    Add Santa dera crewman

---
+++ src/main/java/com/ezardlabs/lostsector/objects/enemies/christmas/corpus/crewmen/SantaDeraCrewman.java
@@ -0,0 +1,24 @@
+package com.ezardlabs.lostsector.objects.enemies.christmas.corpus.crewmen;
+
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.AnimationType;
+import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
+import com.ezardlabs.lostsector.objects.enemies.corpus.crewmen.DeraCrewman;
+
+public class SantaDeraCrewman extends DeraCrewman {
+
+	@Override
+	protected String getAtlasPath() {
+		return "christmas/corpus/crewmen/santa_dera";
+	}
+
+	@Override
+	protected Animation getDieKubrowFrontAnimation() {
+		return new Animation("die_kubrow_front", new Sprite[]{}, AnimationType.ONE_SHOT, 0);
+	}
+
+	@Override
+	protected Animation getDieKubrowBackAnimation() {
+		return new Animation("die_kubrow_back", new Sprite[]{}, AnimationType.ONE_SHOT, 0);
+	}
+}
