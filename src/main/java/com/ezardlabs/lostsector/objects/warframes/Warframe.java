21b8a29c5823ead3b85683292b8d1b2b564b0af7
Ezard <ezard.ben@gmail.com>
2017-07-30 21:11:19 +0100

    Update Warframes to work with animation files

--- src/main/java/com/ezardlabs/lostsector/objects/warframes/Warframe.java
+++ src/main/java/com/ezardlabs/lostsector/objects/warframes/Warframe.java
@@ -1,25 +1,15 @@
 package com.ezardlabs.lostsector.objects.warframes;
 
-import com.ezardlabs.dethsquare.Animation;
-import com.ezardlabs.dethsquare.Animation.AnimationListener;
-import com.ezardlabs.dethsquare.AnimationType;
-import com.ezardlabs.dethsquare.Animator;
-import com.ezardlabs.dethsquare.Camera;
 import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.Renderer;
 import com.ezardlabs.dethsquare.TextureAtlas;
-import com.ezardlabs.dethsquare.TextureAtlas.Sprite;
 import com.ezardlabs.dethsquare.Vector2;
-import com.ezardlabs.dethsquare.multiplayer.Network;
+import com.ezardlabs.dethsquare.animation.Animations;
+import com.ezardlabs.dethsquare.animation.Animations.Validator;
 import com.ezardlabs.lostsector.Game.DamageType;
-import com.ezardlabs.lostsector.map.MapManager;
-import com.ezardlabs.lostsector.objects.CameraMovement;
 import com.ezardlabs.lostsector.objects.ShieldedEntity;
 import com.ezardlabs.lostsector.objects.weapons.MeleeWeapon;
 import com.ezardlabs.lostsector.objects.weapons.RangedWeapon;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public abstract class Warframe extends ShieldedEntity {
 	protected final TextureAtlas ta;
@@ -39,78 +29,9 @@ public abstract class Warframe extends ShieldedEntity {
 	@Override
 	public void start() {
 		gameObject.renderer.setTextureAtlas(ta, 200, 200);
-		gameObject.animator.setAnimations(getIdleAnimation(), getRunAnimation(), getJumpAnimation(), getDoubleJumpAnimation(), getFallAnimation(), getLandAnimation(), getDieAnimation());
+		gameObject.animator.setAnimations(Animations.load(getAnimationPath(), ta,
+				new Validator("idle", "run", "jump", "double-jump", "fall", "land", "die")));
 		gameObject.animator.play("idle");
-	}
-
-	protected Animation getIdleAnimation() {
-		return new Animation("idle", new Sprite[]{ta.getSprite("idle0")}, AnimationType.ONE_SHOT, 0);
-	}
-
-	protected Animation getRunAnimation() {
-		return new Animation("run", new Sprite[]{ta.getSprite("run0"), ta.getSprite("run1"), ta.getSprite("run2"), ta.getSprite("run3"), ta.getSprite("run4"), ta.getSprite("run5")},
-				AnimationType.LOOP, 100);
-	}
-
-	protected Animation getJumpAnimation() {
-		return new Animation("jump", new Sprite[]{ta.getSprite("jump0")}, AnimationType.ONE_SHOT, 0);
-	}
-
-	protected Animation getDoubleJumpAnimation() {
-		return new Animation("doublejump",
-				new Sprite[]{ta.getSprite("jump1"), ta.getSprite("jump2"), ta.getSprite("jump3"), ta.getSprite("jump4"), ta.getSprite("jump5"), ta.getSprite("jump6"), ta.getSprite("jump7")},
-				AnimationType.ONE_SHOT, 50, new AnimationListener() {
-			@Override
-			public void onAnimatedStarted(Animator animator) {
-			}
-
-			@Override
-			public void onFrame(Animator animator, int frameNum) {
-			}
-
-			@Override
-			public void onAnimationFinished(Animator animator) {
-				animator.play("fall");
-			}
-		});
-	}
-
-	protected Animation getFallAnimation() {
-		return new Animation("fall", new Sprite[]{ta.getSprite("fall0"), ta.getSprite("fall1"), ta.getSprite("fall2")}, AnimationType.LOOP, 90);
-	}
-
-	protected Animation getLandAnimation() {
-		return new Animation("land", new Sprite[]{ta.getSprite("land0"), ta.getSprite("land1"), ta.getSprite("land2")}, AnimationType.ONE_SHOT, 100);
-	}
-
-	protected Animation getDieAnimation() {
-		return new Animation("die", new Sprite[]{ta.getSprite("die0"), ta.getSprite("die1"), ta.getSprite("die2"), ta.getSprite("die3"), ta.getSprite("die4")}, AnimationType.ONE_SHOT, 100, new
-				AnimationListener() {
-					@Override
-					public void onAnimatedStarted(Animator animator) {
-						gameObject.renderer.setOffsets(-200, -100);
-						gameObject.renderer.setSize(400, 300);
-					}
-
-					@Override
-					public void onFrame(Animator animator, int frameNum) {
-
-					}
-
-					@Override
-					public void onAnimationFinished(Animator animator) {
-						Network.destroy(gameObject, 2000);
-						new Timer().schedule(new TimerTask() {
-							@Override
-							public void run() {
-								GameObject player = Network
-										.instantiate("player", new Vector2(MapManager.playerSpawn));
-								Camera.main.gameObject.getComponent(CameraMovement.class)
-													  .smoothFollow(player.transform);
-							}
-						}, 2000);
-					}
-				});
 	}
 
 	public abstract String getName();
@@ -122,6 +43,8 @@ public abstract class Warframe extends ShieldedEntity {
 	public abstract void ability3();
 
 	public abstract void ability4();
+
+	protected abstract String getAnimationPath();
 
 	public final void setPrimaryWeapon(RangedWeapon rangedWeapon) {
 		this.rangedWeapon = rangedWeapon;
