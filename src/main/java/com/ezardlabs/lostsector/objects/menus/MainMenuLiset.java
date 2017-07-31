a37c339f80091f2f368a21c599bf072a27cf680e
Ezard <ezard.ben@gmail.com>
2017-07-31 09:48:36 +0100

    Update main menu to work with animation files

--- src/main/java/com/ezardlabs/lostsector/objects/menus/MainMenuLiset.java
+++ src/main/java/com/ezardlabs/lostsector/objects/menus/MainMenuLiset.java
@@ -1,9 +1,10 @@
 package com.ezardlabs.lostsector.objects.menus;
 
 import com.ezardlabs.dethsquare.Animation;
-import com.ezardlabs.dethsquare.AnimationType;
 import com.ezardlabs.dethsquare.Script;
 import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.animation.Animations;
+import com.ezardlabs.dethsquare.animation.Animations.Validator;
 
 public class MainMenuLiset extends Script {
 	private Animation[] animations = null;
@@ -11,31 +12,8 @@ public class MainMenuLiset extends Script {
 	private boolean close = false;
 
 	public MainMenuLiset(boolean close) {
-		ta = new TextureAtlas("images/menus/liset_frost_atlas.png",
-				"images/menus/liset_frost_atlas.txt");
-		animations = new Animation[]{new Animation("liset_frost_open",
-				new TextureAtlas.Sprite[]{ta.getSprite("closed_0"),
-						ta.getSprite("turn_l_1"),
-						ta.getSprite("turn_l_2"),
-						ta.getSprite("turn_l_3"),
-						ta.getSprite("turn_l_4"),
-						ta.getSprite("turn_l_5"),
-						ta.getSprite("turn_l_6"),
-						ta.getSprite("turn_l_7"),
-						ta.getSprite("turn_l_8"),
-						ta.getSprite("turn_l_9"),
-						ta.getSprite("open"),}, AnimationType.ONE_SHOT, 100),
-				new Animation("liset_frost_close", new TextureAtlas.Sprite[]{ta.getSprite("open"),
-						ta.getSprite("turn_l_9"),
-						ta.getSprite("turn_l_8"),
-						ta.getSprite("turn_l_7"),
-						ta.getSprite("turn_l_6"),
-						ta.getSprite("turn_l_5"),
-						ta.getSprite("turn_l_4"),
-						ta.getSprite("turn_l_3"),
-						ta.getSprite("turn_l_2"),
-						ta.getSprite("turn_l_1"),
-						ta.getSprite("closed_0"),}, AnimationType.ONE_SHOT, 100),};
+		ta = new TextureAtlas("images/menus/liset_frost_atlas.png", "images/menus/liset_frost_atlas.txt");
+		animations = Animations.load("ui/main-menu-liset", ta, new Validator("liset_frost_open", "liset_frost_close"));
 
 		this.close = close;
 	}
