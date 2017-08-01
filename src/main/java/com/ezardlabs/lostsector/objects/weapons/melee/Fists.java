5fdf825dc3a0ee5afb9f71ec7ff21e9a51c7fcdf
Ezard <ezard.ben@gmail.com>
2017-08-01 12:00:10 +0100

    Add fists as a melee weapon

---
+++ src/main/java/com/ezardlabs/lostsector/objects/weapons/melee/Fists.java
@@ -0,0 +1,46 @@
+package com.ezardlabs.lostsector.objects.weapons.melee;
+
+import com.ezardlabs.dethsquare.Animation;
+import com.ezardlabs.dethsquare.Animation.AnimationListener;
+import com.ezardlabs.dethsquare.Animator;
+import com.ezardlabs.dethsquare.GameObject;
+import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.animation.Animations;
+import com.ezardlabs.dethsquare.animation.Animations.Validator;
+import com.ezardlabs.lostsector.Game.DamageType;
+import com.ezardlabs.lostsector.objects.Player;
+import com.ezardlabs.lostsector.objects.Player.State;
+import com.ezardlabs.lostsector.objects.weapons.MeleeWeapon;
+
+public class Fists extends MeleeWeapon {
+
+	public Fists(GameObject wielder) {
+		super("Fists", DamageType.NORMAL, wielder);
+	}
+
+	@Override
+	public Animation[] getAnimations(TextureAtlas ta) {
+		Animation[] animations = Animations.load("animations/weapons/melee/fists", ta, new Validator("punch"));
+		animations[0].setAnimationListener(new AnimationListener() {
+			@Override
+			public void onAnimatedStarted(Animator animator) {
+			}
+
+			@Override
+			public void onFrame(Animator animator, int frameNum) {
+			}
+
+			@Override
+			public void onAnimationFinished(Animator animator) {
+				//noinspection ConstantConditions
+				animator.gameObject.getComponent(Player.class).state = State.IDLE;
+			}
+		});
+		return animations;
+	}
+
+	@Override
+	public String getNextAnimation(int direction) {
+		return "punch";
+	}
+}
