73a76d1c8ea27b1a510fda6fff16b85661ffc9b2
Ezard <ezard.ben@gmail.com>
2017-09-03 23:51:59 +0100

    Load defense map and spawn player and camera

--- src/main/java/com/ezardlabs/lostsector/missions/DefenseMission.java
+++ src/main/java/com/ezardlabs/lostsector/missions/DefenseMission.java
@@ -1,5 +1,11 @@
 package com.ezardlabs.lostsector.missions;
 
+import com.ezardlabs.dethsquare.Camera;
+import com.ezardlabs.dethsquare.GameObject;
+import com.ezardlabs.dethsquare.Vector2;
+import com.ezardlabs.dethsquare.prefabs.PrefabManager;
+import com.ezardlabs.lostsector.camera.SmartCamera;
+import com.ezardlabs.lostsector.map.MapManager;
 import com.ezardlabs.lostsector.missions.objectives.Cryopod;
 
 public class DefenseMission extends Mission {
@@ -7,7 +13,10 @@ public class DefenseMission extends Mission {
 
 	@Override
 	public void load() {
-
+		MapManager.loadMap("defense0");
+		GameObject player = GameObject.instantiate(PrefabManager.loadPrefab("player"), MapManager.playerSpawn);
+		GameObject.instantiate(new GameObject("Camera", new Camera(true),
+				new SmartCamera(player.transform, 1000, new Vector2(100, 100))), new Vector2());
 	}
 
 	public void onCryopodDestroyed() {
