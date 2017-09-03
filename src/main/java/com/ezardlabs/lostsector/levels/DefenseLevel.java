9feebe4a0e97e9748c23952745a1390bb7ef3539
Ezard <ezard.ben@gmail.com>
2017-09-04 00:05:57 +0100

    Add defense level

---
+++ src/main/java/com/ezardlabs/lostsector/levels/DefenseLevel.java
@@ -0,0 +1,10 @@
+package com.ezardlabs.lostsector.levels;
+
+import com.ezardlabs.lostsector.missions.DefenseMission;
+
+public class DefenseLevel extends MissionLevel<DefenseMission> {
+
+	public DefenseLevel() {
+		super(new DefenseMission());
+	}
+}
