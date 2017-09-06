8adeaca58c3402479e597b2f898931aed55c79f8
Ezard <ezard.ben@gmail.com>
2017-09-07 00:21:38 +0100

    Only spawn enemies until the maximum limit is reached

--- src/main/java/com/ezardlabs/lostsector/missions/SpawnPoint.java
+++ src/main/java/com/ezardlabs/lostsector/missions/SpawnPoint.java
@@ -1,8 +1,11 @@
 package com.ezardlabs.lostsector.missions;
 
 import com.ezardlabs.dethsquare.GameObject;
+import com.ezardlabs.dethsquare.Level;
+import com.ezardlabs.dethsquare.LevelManager;
 import com.ezardlabs.dethsquare.Script;
 import com.ezardlabs.dethsquare.prefabs.PrefabManager;
+import com.ezardlabs.lostsector.levels.MissionLevel;
 
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
@@ -12,8 +15,10 @@ public class SpawnPoint extends Script {
 	private static final LinkedHashMap<String, Float> enemyChances = new LinkedHashMap<>();
 	private static final long minInterval = 2000;
 	private static final long maxInterval = 20000;
+	private static final int maxEnemies = 20;
 
 	private long nextSpawn = 0;
+	private Mission currentMission;
 
 	static {
 		enemyChances.put("dera_crewman", 0.3f);
@@ -22,11 +27,21 @@ public class SpawnPoint extends Script {
 	}
 
 	@Override
+	public void start() {
+		Level level = LevelManager.getCurrentLevel();
+		if (level instanceof MissionLevel) {
+			currentMission = ((MissionLevel) level).getMission();
+		}
+	}
+
+	@Override
 	public void update() {
 		if (System.currentTimeMillis() > nextSpawn) {
-			String enemyPrefab = getSpawn();
-			if (enemyPrefab != null) {
-				GameObject.instantiate(PrefabManager.loadPrefab(getSpawn()), transform.position);
+			if (currentMission == null || currentMission.numEnemies < maxEnemies) {
+				String enemyPrefab = getSpawn();
+				if (enemyPrefab != null) {
+					GameObject.instantiate(PrefabManager.loadPrefab(getSpawn()), transform.position);
+				}
 			}
 			nextSpawn = System.currentTimeMillis() + ThreadLocalRandom.current().nextLong(minInterval, maxInterval + 1);
 		}
