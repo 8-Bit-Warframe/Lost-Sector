b010385697ce642f3dc40242cbeaf72519d57021
Ezard <ezard.ben@gmail.com>
2017-09-07 21:39:22 +0100

    Update constant variable names to comply with naming conventions

--- src/main/java/com/ezardlabs/lostsector/missions/SpawnPoint.java
+++ src/main/java/com/ezardlabs/lostsector/missions/SpawnPoint.java
@@ -12,18 +12,18 @@ import java.util.Map.Entry;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SpawnPoint extends Script {
-	private static final LinkedHashMap<String, Float> enemyChances = new LinkedHashMap<>();
-	private static final long minInterval = 2000;
-	private static final long maxInterval = 20000;
-	private static final int maxEnemies = 20;
+	private static final LinkedHashMap<String, Float> ENEMY_CHANCES = new LinkedHashMap<>();
+	private static final long MIN_INTERVAL = 2000;
+	private static final long MAX_INTERVAL = 20000;
+	private static final int MAX_ENEMIES = 20;
 
 	private long nextSpawn = 0;
 	private Mission currentMission;
 
 	static {
-		enemyChances.put("dera_crewman", 0.3f);
-		enemyChances.put("prova_crewman", 0.5f);
-		enemyChances.put("supra_crewman", 0.2f);
+		ENEMY_CHANCES.put("dera_crewman", 0.3f);
+		ENEMY_CHANCES.put("prova_crewman", 0.5f);
+		ENEMY_CHANCES.put("supra_crewman", 0.2f);
 	}
 
 	@Override
@@ -37,13 +37,13 @@ public class SpawnPoint extends Script {
 	@Override
 	public void update() {
 		if (System.currentTimeMillis() > nextSpawn) {
-			if (currentMission == null || currentMission.numEnemies < maxEnemies) {
+			if (currentMission == null || currentMission.numEnemies < MAX_ENEMIES) {
 				String enemyPrefab = getSpawn();
 				if (enemyPrefab != null) {
 					GameObject.instantiate(PrefabManager.loadPrefab(getSpawn()), transform.position);
 				}
 			}
-			nextSpawn = System.currentTimeMillis() + ThreadLocalRandom.current().nextLong(minInterval, maxInterval + 1);
+			nextSpawn = System.currentTimeMillis() + ThreadLocalRandom.current().nextLong(MIN_INTERVAL, MAX_INTERVAL + 1);
 		}
 	}
 
@@ -51,7 +51,7 @@ public class SpawnPoint extends Script {
 		double rand = Math.random();
 		float total = 0;
 
-		for (Entry<String, Float> entry : enemyChances.entrySet()) {
+		for (Entry<String, Float> entry : ENEMY_CHANCES.entrySet()) {
 			if (rand < entry.getValue() + total) {
 				return entry.getKey();
 			} else {
