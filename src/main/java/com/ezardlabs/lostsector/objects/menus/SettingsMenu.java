5421d0ffe74491957e2a47c523dd63a93d90fcac
Ezard <ezard.ben@gmail.com>
2017-09-27 16:21:23 +0100

    Refactor to make use of new Dethsquare features (object hierarchies and pausing)

--- src/main/java/com/ezardlabs/lostsector/objects/menus/SettingsMenu.java
+++ src/main/java/com/ezardlabs/lostsector/objects/menus/SettingsMenu.java
@@ -8,133 +8,161 @@ import com.ezardlabs.dethsquare.Input;
 import com.ezardlabs.dethsquare.Input.KeyCode;
 import com.ezardlabs.dethsquare.Script;
 import com.ezardlabs.dethsquare.TextureAtlas;
+import com.ezardlabs.dethsquare.Time;
 import com.ezardlabs.dethsquare.Vector2;
-
-import java.util.HashMap;
 
 public class SettingsMenu extends Script {
 	private static final String AUDIO_MASTER_VOLUME = "audio_master_volume";
 	private static final String AUDIO_MUSIC_VOLUME = "audio_music_volume";
 	private static final String AUDIO_SFX_VOLUME = "audio_sfx_volume";
 	private static final TextureAtlas FONT = TextureAtlas.load("fonts/atlas.png", "fonts/atlas.txt");
-	private final SettingsTab audio = new SettingsTab();
 	private final BackButtonAction backButtonAction;
-	private final SettingsTab graphics = new SettingsTab();
-	private final SettingsTab controls = new SettingsTab();
-	private final GuiText audioText = new GuiText("AUDIO", FONT, 31.25f);
-	private final GuiText graphicsText = new GuiText("GRAPHICS", FONT, 31.25f);
-	private final GuiText controlsText = new GuiText("CONTROLS", FONT, 31.25f);
 	private final GuiText menuText = new GuiText("MENU", FONT, 31.25f);
-	private final GameObject[] circles = new GameObject[3];
 
-	public SettingsMenu(BackButtonAction backButtonAction) {
+	SettingsMenu(BackButtonAction backButtonAction) {
 		this.backButtonAction = backButtonAction;
 	}
 
 	@Override
 	public void start() {
+		runWhenPaused = true;
 		GameObject.instantiate(
 				new GameObject("Settings Background", new GuiRenderer("images/menus/settings/base.png", 1200, 1000)),
-				transform.position);
+				transform.position).transform.setParent(transform);
 
 		GameObject.instantiate(
 				new GameObject("Menu Button", new GuiRenderer("images/menus/settings/menu.png", 575, 118.75f)),
-				new Vector2(transform.position.offset(550, 881.25f)));
+				new Vector2(transform.position.offset(550, 881.25f))).transform.setParent(transform);
 
 		GameObject.instantiate(new GameObject("Menu Button Text", menuText), new Vector2(
-				transform.position.offset(837.5f - menuText.getWidth() / 2, 943.75f - menuText.getFontSize() / 2)));
+				transform.position.offset(837.5f - menuText.getWidth() / 2,
+						943.75f - menuText.getFontSize() / 2))).transform.setParent(transform);
 
-		for (int i = 0; i < circles.length; i++) {
-			circles[i] = GameObject.instantiate(new GameObject("Circle " + i,
-							new GuiRenderer("images/menus/settings/circle" + i + ".png", 81.25f, 81.25f)),
-					new Vector2(10000, 10000));
-		}
-		circles[0].transform.position.set(transform.position.offset(1106.25f, 125));
-
-		setupAudioTab();
-		setupGraphicsTab();
-		setupControlsTab();
+		GameObject.instantiate(new GameObject("Settings Tab Manager",
+						new SettingsTabManager(new AudioSettingsTab(), new GraphicsSettingsTab(), new ControlsSettingsTab())),
+				new Vector2()).transform.setParent(transform);
+		gameObject.setActive(false);
 	}
 
 	@Override
 	public void update() {
-		if (Input.getKeyDown(KeyCode.MOUSE_LEFT) && audioText.hitTest(Input.mousePosition)) {
-			audio.show();
-			graphics.hide();
-			controls.hide();
-			circles[0].transform.position.set(transform.position.offset(1106.25f, 125));
-			circles[1].transform.position.set(transform.position.offset(10000, 10000));
-			circles[2].transform.position.set(transform.position.offset(10000, 10000));
-		}
-		if (Input.getKeyDown(KeyCode.MOUSE_LEFT) && graphicsText.hitTest(Input.mousePosition)) {
-			audio.hide();
-			graphics.show();
-			controls.hide();
-			circles[0].transform.position.set(transform.position.offset(10000, 10000));
-			circles[1].transform.position.set(transform.position.offset(1106.25f, 125));
-			circles[2].transform.position.set(transform.position.offset(10000, 10000));
-		}
-		if (Input.getKeyDown(KeyCode.MOUSE_LEFT) && controlsText.hitTest(Input.mousePosition)) {
-			audio.hide();
-			graphics.hide();
-			controls.show();
-			circles[0].transform.position.set(transform.position.offset(10000, 10000));
-			circles[1].transform.position.set(transform.position.offset(10000, 10000));
-			circles[2].transform.position.set(transform.position.offset(1106.25f, 125));
-		}
 		if (Input.getKeyDown(KeyCode.MOUSE_LEFT) && menuText.hitTest(Input.mousePosition)) {
 			backButtonAction.onBackButtonClicked(this);
 		}
+		Time.pause();
 	}
 
-	private void setupAudioTab() {
-		GameObject.instantiate(new GameObject("Audio Tab", audioText),
-				new Vector2(transform.position.x + 212.5f - audioText.getWidth() / 2f,
-						transform.position.y + 77.5f - audioText.getFontSize() / 2));
+	private class SettingsTabManager extends Script {
+		private final SettingsTab[] tabs;
+		private final GuiText[] titles = new GuiText[3];
+		private final GameObject[] roots = new GameObject[3];
 
-		audio.addObject(new GameObject("Master Audio Slider",
-						new Slider(0, 15, 5, "MASTER", "images/menus/settings/slider_icon_audio_master" + ".png",
-								AUDIO_MASTER_VOLUME, newValue -> AudioManager.setMasterVolume(newValue / 15f))),
-				new Vector2(transform.position.x + 206.25f, transform.position.y + 256.25f));
-		audio.addObject(new GameObject("Music Audio Slider",
-						new Slider(0, 15, 5, "MUSIC", "images/menus/settings/slider_icon_audio_music" + ".png",
-								AUDIO_MUSIC_VOLUME, newValue -> AudioManager.setMusicVolume(newValue / 15f))),
-				new Vector2(transform.position.x + 206.25f, transform.position.y + 456.25f));
-		audio.addObject(new GameObject("SFX Audio Slider",
-						new Slider(0, 15, 5, "SOUND EFFECTS", "images/menus/settings/slider_icon_audio_sfx.png",
-								AUDIO_SFX_VOLUME, newValue -> AudioManager.setSfxVolume(newValue / 15f))),
-				new Vector2(transform.position.x + 206.25f, transform.position.y + 656.25f));
-	}
-
-	private void setupGraphicsTab() {
-		GameObject.instantiate(new GameObject("Graphics Tab", graphicsText),
-				new Vector2(transform.position.x + 556.25f - graphicsText.getWidth() / 2f,
-						transform.position.y + 77.5f - graphicsText.getFontSize() / 2));
-	}
-
-	private void setupControlsTab() {
-		GameObject.instantiate(new GameObject("Controls Tab", controlsText),
-				new Vector2(transform.position.x + 900 - controlsText.getWidth() / 2f,
-						transform.position.y + 77.5f - controlsText.getFontSize() / 2));
-	}
-
-	private class SettingsTab {
-		private HashMap<GameObject, Vector2> objects = new HashMap<>();
-
-		private void addObject(GameObject object, Vector2 position) {
-			objects.put(GameObject.instantiate(object, position), position);
+		private SettingsTabManager(SettingsTab tab1, SettingsTab tab2, SettingsTab tab3) {
+			this.tabs = new SettingsTab[]{
+					tab1,
+					tab2,
+					tab3
+			};
 		}
 
-		private void show() {
-			for (GameObject go : objects.keySet()) {
-				go.transform.position.set(objects.get(go));
+		@Override
+		public void start() {
+			runWhenPaused = true;
+			for (int i = 0; i < tabs.length; i++) {
+				titles[i] = new GuiText(tabs[i].getTitle(), FONT, 31.25f);
+				GameObject.instantiate(new GameObject(null, titles[i]),
+						transform.position.offset(212.5f + (i * 343.75f) - (titles[i].getWidth() / 2f),
+								77.5f - (titles[i].getFontSize() / 2))).transform.setParent(transform);
+				roots[i] = tabs[i].getRootObject();
+				roots[i].transform.setParent(transform);
+				if (i > 0) {
+					roots[i].setActive(false);
+				}
+				GameObject.instantiate(new GameObject("Circle " + i,
+								new GuiRenderer("images/menus/settings/circle" + i + ".png", 81.25f, 81.25f)),
+						new Vector2(1106.25f, 125)).transform.setParent(roots[i].transform);
 			}
 		}
 
-		private void hide() {
-			for (GameObject go : objects.keySet()) {
-				go.transform.position.set(10000, 10000);
+		@Override
+		public void update() {
+			if (Input.getKeyDown(KeyCode.MOUSE_LEFT)) {
+				boolean hit = false;
+				int index = -1;
+				for (int i = 0; i < titles.length; i++) {
+					if (titles[i].hitTest(Input.mousePosition)) {
+						hit = true;
+						index = i;
+						break;
+					}
+				}
+				if (hit) {
+					for (int i = 0; i < titles.length; i++) {
+						roots[i].setActive(i == index);
+					}
+				}
 			}
+		}
+	}
+
+	private interface SettingsTab {
+		String getTitle();
+
+		GameObject getRootObject();
+	}
+
+	private class AudioSettingsTab implements SettingsTab {
+
+		@Override
+		public String getTitle() {
+			return "AUDIO";
+		}
+
+		@Override
+		public GameObject getRootObject() {
+			GameObject root = GameObject.instantiate(new GameObject(), new Vector2());
+			GameObject.instantiate(new GameObject("Master Audio Slider",
+							new Slider(0, 15, 5, "MASTER", "images/menus/settings/slider_icon_audio_master" + ".png",
+									AUDIO_MASTER_VOLUME, newValue -> AudioManager.setMasterVolume(newValue / 15f))),
+					new Vector2(transform.position.x + 206.25f, transform.position.y + 256.25f)).transform.setParent(
+					root.transform);
+			GameObject.instantiate(new GameObject("Music Audio Slider",
+							new Slider(0, 15, 5, "MUSIC", "images/menus/settings/slider_icon_audio_music" + ".png",
+									AUDIO_MUSIC_VOLUME, newValue -> AudioManager.setMusicVolume(newValue / 15f))),
+					new Vector2(transform.position.x + 206.25f, transform.position.y + 456.25f)).transform.setParent(
+					root.transform);
+			GameObject.instantiate(new GameObject("SFX Audio Slider",
+							new Slider(0, 15, 5, "SOUND EFFECTS", "images/menus/settings/slider_icon_audio_sfx.png",
+									AUDIO_SFX_VOLUME, newValue -> AudioManager.setSfxVolume(newValue / 15f))),
+					new Vector2(transform.position.x + 206.25f, transform.position.y + 656.25f)).transform.setParent(
+					root.transform);
+			return root;
+		}
+	}
+
+	private class GraphicsSettingsTab implements SettingsTab {
+
+		@Override
+		public String getTitle() {
+			return "GRAPHICS";
+		}
+
+		@Override
+		public GameObject getRootObject() {
+			return GameObject.instantiate(new GameObject(), new Vector2());
+		}
+	}
+
+	private class ControlsSettingsTab implements SettingsTab {
+
+		@Override
+		public String getTitle() {
+			return "CONTROLS";
+		}
+
+		@Override
+		public GameObject getRootObject() {
+			return GameObject.instantiate(new GameObject(), new Vector2());
 		}
 	}
 
