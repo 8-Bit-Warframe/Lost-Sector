97f860b85b81a8dfca48c7b53be711f1c3263df6
Ezard <ezard.ben@gmail.com>
2017-10-01 16:49:40 +0100

    Add bullets to Gorgon

--- src/main/java/com/ezardlabs/lostsector/objects/weapons/primary/Gorgon.java
+++ src/main/java/com/ezardlabs/lostsector/objects/weapons/primary/Gorgon.java
@@ -1,8 +1,12 @@
 package com.ezardlabs.lostsector.objects.weapons.primary;
 
+import com.ezardlabs.dethsquare.Collider;
+import com.ezardlabs.dethsquare.GameObject;
 import com.ezardlabs.dethsquare.Input;
 import com.ezardlabs.dethsquare.Input.KeyCode;
 import com.ezardlabs.dethsquare.animation.AnimationType;
+import com.ezardlabs.dethsquare.graphics.Renderer;
+import com.ezardlabs.lostsector.objects.projectiles.Laser;
 import com.ezardlabs.lostsector.objects.weapons.PrimaryWeapon;
 
 public class Gorgon extends PrimaryWeapon {
@@ -33,6 +37,11 @@ public class Gorgon extends PrimaryWeapon {
 							return currentFrame + 1;
 						}
 					case 2:
+						GameObject.instantiate(
+								new GameObject("Gorgon Bullet", new Renderer("images/laser_yellow.png", 100, 100),
+										new Collider(100, 100, true), new Laser(1, "enemy")),
+								transform.position.offset(transform.scale.x > 0 ? 225f : -100,
+										62.5f)).transform.scale.x = transform.scale.x;
 					case 3:
 					case 4:
 					case 5:
